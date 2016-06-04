2e5c6a3e7d3891e15eb3562f09d533a4e1890a92
IzikLisbon <izikl@live.com>
2016-06-04 05:00:24 -0700

    initial commit

---
+++ SearchableTextView/SearchableTextView.h
@@ -0,0 +1,19 @@
+//
+//  SearchableTextView.h
+//  SearchableTextView
+//
+//  Created by Izik Lisbon on 6/4/16.
+//  Copyright © 2016 Izik Lisbon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SearchableTextView.
+FOUNDATION_EXPORT double SearchableTextViewVersionNumber;
+
+//! Project version string for SearchableTextView.
+FOUNDATION_EXPORT const unsigned char SearchableTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SearchableTextView/PublicHeader.h>
+
+
